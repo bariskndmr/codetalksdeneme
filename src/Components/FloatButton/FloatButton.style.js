a58f4fccb3b4342a060323bf94be1c1633f68c7f
Barış Kandemir <48553738+bariskndmr@users.noreply.github.com>
2022-03-13 23:08:57 +0300

    modal

---
+++ src/Components/FloatButton/FloatButton.style.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    right: 20,
+    bottom: 30,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+  },
+});
--- src/Pages/Rooms/Rooms.js
+++ src/Pages/Rooms/Rooms.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, FlatList} from 'react-native';
+
+import FloatButton from 'src/Components/FloatButton';
 import RoomCard from 'src/Components/RoomCard';
+import RoomModal from 'src/Components/RoomModal';
 
 import Styles from './Rooms.style';
 
@@ -17,12 +20,33 @@ const roomListRender = item => {
 };
 
 const Rooms = () => {
+  const [roomModalVisible, setRoomModalVisible] = useState(false);
+
+  const handleModalToggle = () => {
+    setRoomModalVisible(!roomModalVisible);
+  };
+
+  const handleSentRoomName = roomName => {
+    handleModalToggle();
+    sentRoomName(roomName);
+  };
+
+  const sentRoomName = () => {
+    null;
+  };
+
   return (
     <SafeAreaView style={Styles.container}>
       <FlatList
         data={RoomList}
         renderItem={({item}) => roomListRender(item)}
         numColumns={2}
+      />
+      <FloatButton text="+" onPress={handleModalToggle} />
+      <RoomModal
+        visible={roomModalVisible}
+        onClose={handleModalToggle}
+        onSend={handleSentRoomName}
       />
     </SafeAreaView>
   );
