62f86965c21058d6cd3e736445ad4a53885e8269
Barış Kandemir <48553738+bariskndmr@users.noreply.github.com>
2022-03-15 16:02:58 +0300

    al hamza

--- src/Pages/Rooms/Rooms.js
+++ src/Pages/Rooms/Rooms.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {SafeAreaView, FlatList} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 
 import FloatButton from 'src/Components/FloatButton';
 import RoomCard from 'src/Components/RoomCard';
@@ -7,20 +9,21 @@ import RoomModal from 'src/Components/RoomModal';
 
 import Styles from './Rooms.style';
 
-const RoomList = [
-  {text: 'React-Native'},
-  {text: 'React'},
-  {text: 'Python'},
-  {text: 'Python'},
-  {text: 'Python'},
-];
-
-const roomListRender = item => {
-  return <RoomCard text={item.text} />;
-};
-
 const Rooms = () => {
   const [roomModalVisible, setRoomModalVisible] = useState(false);
+  const [roomList, setRoomList] = useState([]);
+
+  React.useEffect(() => {
+    database()
+      .ref('Rooms/')
+      .on('value', snapshot => {
+        setRoomList(snapshot.val());
+      });
+  }, []);
+
+  const roomListRender = item => {
+    return <RoomCard text={item.roomName} />;
+  };
 
   const handleModalToggle = () => {
     setRoomModalVisible(!roomModalVisible);
@@ -31,14 +34,21 @@ const Rooms = () => {
     sentRoomName(roomName);
   };
 
-  const sentRoomName = () => {
-    null;
+  const sentRoomName = roomName => {
+    const userEmail = auth().currentUser.email;
+
+    const contentObject = {
+      roomName: roomName,
+      userName: userEmail.split('@')[0],
+    };
+
+    database().ref('Rooms/').push(contentObject);
   };
 
   return (
     <SafeAreaView style={Styles.container}>
       <FlatList
-        data={RoomList}
+        data={roomList}
         renderItem={({item}) => roomListRender(item)}
         numColumns={2}
       />
